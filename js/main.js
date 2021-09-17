da150933fcca33ddb9425c0eb80d59d5baefc742
Ethan <spaminol@gmail.com>
2021-09-16 17:37:20 -0700

    Show scrollbar on lightbox open

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 import { Fancybox } from "@fancyapps/ui/src/Fancybox/Fancybox.js";
 import "@fancyapps/ui/dist/fancybox.css";
 
-Fancybox.bind('[data-fancybox="gallery"]')
+Fancybox.bind('[data-fancybox="gallery"]', {
+  hideScrollbar: false
+})
